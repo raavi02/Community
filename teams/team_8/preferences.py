70cecde26aeb46503fecdacbe5039c7be3131f49
jx2518 <121977360+jx2518@users.noreply.github.com>
2024-11-18 14:07:57 -0500

    fixed a bug bin pointed out in preferences.py

--- teams/team_8/preferences.py
+++ teams/team_8/preferences.py
@@ -45,10 +45,11 @@ def phaseIpreferences(player, community, global_random):
         # which means solo work is prohibitively expensive, meaing teamwork is more favorable.
         if (
             W_solo >= 1.5 * min_W_team
+            # and player.energy - min_W_team >= 0
+            # and partner.energy - min_W_team >= 0
             ):
             preferences.append([task_index, best_partner])
-        else:
-            return []
+
         
     #print("Player ", player.id, "'s preference is: ", preferences)
     return preferences
